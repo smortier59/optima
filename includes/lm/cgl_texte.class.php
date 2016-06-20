06e1caae15b2b391fed921d3c9451d6f284139d8
Morgan <mfleurquin@absystech.fr>
2016-06-20 16:17:53 +0200

    CGL

---
+++ includes/lm/cgl_texte.class.php
@@ -0,0 +1,25 @@
+<?
+/** Classe cgl_texte
+* @package Optima
+* @subpackage Cléodis
+*/
+class cgl_texte extends classes_optima {
+	public function __construct() { 
+		parent::__construct();
+		$this->table = __CLASS__;
+		$this->colonnes["fields_column"] = array(	
+			 'cgl_texte.id_cgl_texte'			 
+			,"cgl_texte.numero"
+			,'cgl_texte.titre'
+			,'cgl_texte.id_cgl_article'
+		);
+		
+		$this->colonnes["primary"] = array('id_cgl_texte','titre','numero');
+		$this->panels['primary'] = array("visible"=>true, 'nbCols'=>1);
+
+		$this->field_nom = "titre";
+
+		$this->fieldstructure();
+	}
+};
+?>
