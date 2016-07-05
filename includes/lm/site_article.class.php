e9fb5a64eb5c94fdf65e62f6f13d2ad6b154a7cc
Morgan <mfleurquin@absystech.fr>
2016-07-05 10:17:48 +0200

    Correctif Site article

---
+++ includes/lm/site_article.class.php
@@ -0,0 +1,28 @@
+<?
+/**
+* Classe site_article
+* @package Optima
+*/
+class site_article extends classes_optima {
+	/**
+	* Constructeur
+	*/
+	public function __construct() { 
+		parent::__construct();
+		$this->table = "site_article";
+
+		$this->colonnes['fields_column'] = array(
+			 'site_article.id_site_menu'
+			,'site_article.id_parent'
+			,'site_article.titre'			
+			,'site_article.position'			
+            ,'site_article.visible'
+		);
+
+
+		$this->field_nom = "titre";
+		$this->fieldstructure();	
+		$this->foreign_key["id_parent"] = "site_article";
+
+	}
+}
