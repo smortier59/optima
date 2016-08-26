25556abc6fc84d33a478655138b71b166d482d17
Morgan <mfleurquin@absystech.fr>
2016-08-26 15:44:43 +0200

    suite modif

--- includes/lm/comite.class.php
+++ includes/lm/comite.class.php
@@ -391,6 +391,9 @@ class comite extends classes_optima {
     * @return array
     */
     public static function getInfosCommande($id_commande, $id_fournisseur){ // fonction get accessible uniquement par le prestataire
+       
+    	$element_declencheur = "acceptation_comite";
+		$produit_declencheur = array();
         $q = "SELECT 
                 commande.id_commande, 
                 commande.ref, 
@@ -414,12 +417,18 @@ class comite extends classes_optima {
         $q = "SELECT * FROM commande_ligne WHERE id_fournisseur='".ATF::db()->real_escape_string($id_fournisseur)."' AND id_commande='".ATF::db()->real_escape_string($id_commande)."'";
         if ($all_lines = ATF::db()->arr($q)) { // on recupère toutes ses lignes
             foreach ($all_lines as $key_lines => $lc) { // pour chaque lignes de la commande
-                $q = "SELECT produit, id_produit, ref_lm FROM produit WHERE id_produit='".ATF::db()->real_escape_string($lc['id_produit'])."'";
+                $q = "SELECT produit, id_produit, ref_lm, element_declencheur FROM produit WHERE id_produit='".ATF::db()->real_escape_string($lc['id_produit'])."'";
                 $p = ATF::db()->fasso($q);
                 $p["quantite"] = $lc["quantite"];
+                if($p["element_declencheur"] == $element_declencheur) $produit_declencheur[] = $p['produit']." (id_produit : ".$p["id_produit"].")";
+                unset($p["element_declencheur"]);
+
                 $cmd['produit'][] = $p; // on ajout dans la commande sur laquelle on itère le produit en question dans un tableau 'produit'
+            	
+            	
             }
         }
+        $cmd["produit_declencheur"] = $produit_declencheur;
         return $cmd;
     } 
 };
