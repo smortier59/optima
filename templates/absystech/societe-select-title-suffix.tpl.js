0a415b42fe885cd4122f59555ce07f3a6a054abe
Morgan <mfleurquin@absystech.fr>
2016-11-25 09:04:06 +0100

    15106 - Ajout des informations sur le contrat de maintenance sur la fiche société

--- templates/absystech/societe-select-title-suffix.tpl.js
+++ templates/absystech/societe-select-title-suffix.tpl.js
@@ -350,3 +350,56 @@
 			window.open("societe,atcard.ajax,id_societe={$id}");
 		}
 	}
+
+
+{$societe = ATF::societe()->select($infos.id_societe)}
+
+{if $societe["est_sous_contrat_maintenance"]}
+	{if $societe["est_sous_contrat_maintenance"] == "sous_contrat"}
+		{$alerte = "btn-success"}
+		{$texte = "Client sous contrat de maintenance"}
+	{else if $societe["est_sous_contrat_maintenance"] == "sous_contrat_partiel"}
+		{$alerte = "btn-warning"}
+		{$texte = "Contrat de maintenance Partiel"}
+	{else}
+		{$alerte = "btn-danger"}
+		{$texte = "Le client ne possède pas de contrat de maintenance"}
+	{/if}
+
+	{$commentaire = ""}
+	{if $societe["commentaire_contrat_maintenance"]}
+		{$commentaire = $societe["commentaire_contrat_maintenance"]}
+	{/if}
+
+	{$image =''}
+	{if $societe["option_contrat_maintenance"] != "aucune"}
+		{if $societe["option_contrat_maintenance"] == "a_caliner"}
+			,{
+				html: '<button type="button" class="btn btn-lg {$alerte}"><div style="float:left; margin-right:10px;"><img src="{ATF::$staticserver}images/heart.png" style="height:40px;"></div>{$texte}{if $commentaire}<br><span class="infos_sup">{$commentaire|escape:javascript}</span>{/if}</button>'
+				,border: false
+				,style: {
+					marginLeft: "10px",
+					float: 'left'
+				}
+			}		
+		{else}
+			,{
+				html: '<button type="button" class="btn btn-lg {$alerte}"><div style="float:left; margin-right:10px;"><img src="{ATF::$staticserver}images/new_install.png" style="height:40px;"></div>{$texte}{if $commentaire}<br><span class="infos_sup">{$commentaire|escape:javascript}</span>{/if}</button>'
+				,border: false
+				,style: {
+					marginLeft: "10px",
+					float: 'left'
+				}
+			}		
+		{/if}
+	{else}
+		,{
+			html: '<button type="button" class="btn btn-lg {$alerte}">{$image}{$texte}{if $commentaire}<br><span class="infos_sup">{$commentaire|escape:javascript}</span>{/if}</button>'
+			,border: false
+			,style: {
+				marginLeft: "10px",
+				float: 'left'
+			}
+		}
+	{/if}
+{/if}
