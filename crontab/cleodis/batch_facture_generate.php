186424ad15fee8c4189e1c7da7458f4cd4236db7
ccharlier <ccharlier@absystech.fr>
2017-01-17 11:53:20 +0100

    crontab facture juillet cleodisbe

--- crontab/cleodis/batch_facture_generate.php
+++ crontab/cleodis/batch_facture_generate.php
@@ -1,23 +1,14 @@
 <?php
 define("__BYPASS__",true);
-$_SERVER["argv"][1] = "cleodis";
+$_SERVER["argv"][1] = "cleodisbe";
 include(dirname(__FILE__)."/../../global.inc.php");
 ATF::define("tracabilite",false);
 
-
-$factures = array( '1508001-4'
-,'1604001-2'
-,'1606001-1'
-,'1404040-21'
-,'1407020-21'
-,'1502041-11'
-,'1505002-10'
-,'1506001-13'
-,'1602004-1');
-
+$req = " SELECT id_facture, date FROM facture WHERE `date` BETWEEN '2016-07-01' AND '2016-07-31' ";
+$factures = ATF::db()->sql2array($req);
 foreach ($factures as $key => $value) {
-  $fact['id'] = $value;
+  $fact['id'] = $value['id_facture'];
   ATF::facture()->genererPdf($fact);
-  log::logger('id facture -- '.$value,"ccharlier");
+  //log::logger('id facture -- '.$value['id_facture'],"ccharlier");
 }
 ?>
