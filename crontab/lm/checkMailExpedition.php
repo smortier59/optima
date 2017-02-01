f08bb7884094aaef35b34a868f3b388b566919e7
Morgan <mfleurquin@absystech.fr>
2017-02-01 09:35:20 +0100

    Ajout du mot de passe suivi_commande LM avec la constante

--- crontab/lm/checkMailExpedition.php
+++ crontab/lm/checkMailExpedition.php
@@ -3,9 +3,9 @@ define("__BYPASS__",true);
 include(dirname(__FILE__)."/../../global.inc.php");
 
 $mail = 'suivicommandes@abonnement.leroymerlin.fr';
-$host = "outlook.office365.com"; 
+$host = "outlook.office365.com";
 $port = 993;
-$password = "m5q9Z6=F_*Wm";
+$password = __SUIVI_COMMANDE_MAILBOX__;
 ATF::affaire()->checkMailBoxExpeditionCommande($mail, $host, $port, $password);
 
 ?>
