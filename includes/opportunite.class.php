7b897c5f11a0a5005f9cf73b0e8b114aeee04903
Morgan <mfleurquin@absystech.fr>
2017-02-21 11:52:34 +0100

    DEVIS/AFFAIRE Telescope
    TU Hotline Absystech

--- includes/opportunite.class.php
+++ includes/opportunite.class.php
@@ -7,14 +7,14 @@ class opportunite extends classes_optima {
 		parent::__construct();
 		$this->table = __CLASS__;
 
-		$this->colonnes["fields_column"] = array(	
+		$this->colonnes["fields_column"] = array(
 			'opportunite.id_societe'
 			,'opportunite.opportunite'
 			,'opportunite.etat'=>array("width"=>30,"align"=>"center","renderer"=>"etat")
 			,'opportunite.date'=>array("width"=>100,"align"=>"center")
 			,'opportunite.id_owner'
 		);
-		
+
 		$this->colonnes['primary'] = array(
 												'id_societe'
 												,'opportunite'
@@ -32,6 +32,30 @@ class opportunite extends classes_optima {
 													);
 		$this->fieldstructure();
 		$this->panels['detail'] = array("visible"=>true);
+	}
+
+	/** Fonction qui génère les résultat pour les champs d'auto complétion opportunite
+	* @author Morgan FLEURQUIN <mfleurquin@absystech.fr>
+	*/
+	public function _ac($get,$post) {
+		$length = 25;
+		$start = 0;
+
+		$this->q->reset();
+
+		// On ajoute les champs utiles pour l'autocomplete
+		$this->q->addField("id_opportunite")->addField("opportunite");
+
+		if ($get['q']) {
+			$this->q->setSearch($get["q"]);
+		}
+
+		if ($get['id_societe']) {
+			$this->q->where("id_societe",$get["id_societe"]);
+		}
+		$this->q->setLimit($length,$start)->setPage($start/$length);
+
+		return $this->select_all();
 	}
 };
 ?>
