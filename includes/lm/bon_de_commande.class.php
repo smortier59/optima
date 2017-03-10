712a7b4e06ff73b50e6c8252c28e6698df759200
Morgan <mfleurquin@absystech.fr>
2017-03-10 15:07:17 +0100

    Correctif insert BDC

--- includes/lm/bon_de_commande.class.php
+++ includes/lm/bon_de_commande.class.php
@@ -567,9 +567,9 @@ class bon_de_commande_lm extends bon_de_commande {
 			$bon_de_commande_ligne["id_bon_de_commande"]=$last_id;
 			$bon_de_commande_ligne["ref"]=$commande_ligne["ref"];
 			$bon_de_commande_ligne["produit"]=$commande_ligne["produit"];
-			$bon_de_commande_ligne["quantite"]=$commande_ligne["quantite"];
+			$bon_de_commande_ligne["quantite"]=1;
 			$bon_de_commande_ligne["prix"]=$commande_ligne["prix_achat"];
-			$prix_total += $bon_de_commande_ligne["prix"]*$bon_de_commande_ligne["quantite"];
+			$prix_total += $bon_de_commande_ligne["prix"];
 			ATF::bon_de_commande_ligne()->i($bon_de_commande_ligne);
 		}
 
