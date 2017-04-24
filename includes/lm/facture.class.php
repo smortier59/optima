0b952de67b04c77aa76ea81231fc9fa78a78ad7f
Morgan <mfleurquin@absystech.fr>
2017-04-24 10:56:20 +0200

    LOG Retour API SLIMPAY

--- includes/lm/facture.class.php
+++ includes/lm/facture.class.php
@@ -217,7 +217,7 @@ class facture_lm extends facture {
 				$facture = $this->select($value["facture.id_facture"]);
 				$status = ATF::slimpay()->getStatutDebit($facture["id_slimpay"]);
 
-				log::logger("Paiement : ".$facture["id_slimpay"]."  ---> ".$status , "StatutDebitSlimpay");
+				log::logger("Paiement : ".$facture["id_slimpay"]."  ---> ".$status["executionStatus"] , "StatutDebitSlimpay");
 
 				if($facture["executionStatus"] !== $status["executionStatus"]){
 					$this->u(array("id_facture"=>$facture["id_facture"],
