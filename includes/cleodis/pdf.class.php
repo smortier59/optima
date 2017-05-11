38da15e5047e6943349a4b8e354e5d007f4eb4e4
Morgan <mfleurquin@absystech.fr>
2017-05-11 10:49:38 +0200

    Correctif PDF CLEODIS en live

--- includes/cleodis/pdf.class.php
+++ includes/cleodis/pdf.class.php
@@ -2830,7 +2830,15 @@ class pdf_cleodis extends pdf {
 			$this->multicell(0,5,"ARTICLE ".$numArticle." : CONDITION DE PAIEMENT ET ECHEANCE");
 			$numArticle++;
 			$this->setfont('arial','',8);
-			$this->multicell(0,5,"La facture est payable par chèque à 30 jours date de facture.");
+
+			if($this->commande['type'] == "virement"){
+				$this->multicell(0,5,"La facture est payable par virement.");
+			} elseif($this->commande['type'] == "cheque"){
+				$this->multicell(0,5,"La facture est payable par chèque à 30 jours date de facture.");
+			}else {
+				$this->multicell(0,5,"La facture est payable par prélèvement.");
+			}
+
 
 
 		} else {
