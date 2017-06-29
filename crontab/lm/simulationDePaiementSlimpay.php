91d8315c62a7c89eaf102e5efb8d04cbf6b2e2bf
Morgan <mfleurquin@absystech.fr>
2017-06-29 14:11:03 +0200

    Simulateur paiement impayee 15729

--- crontab/lm/simulationDePaiementSlimpay.php
+++ crontab/lm/simulationDePaiementSlimpay.php
@@ -4,9 +4,11 @@ include(dirname(__FILE__)."/../../global.inc.php");
 
 
 $refMandate = $_SERVER['argv'][2];
+$montant = $_SERVER['argv'][3];
 
 
-ATF::slimpay()->simulateIssue($refMandate);
+
+ATF::slimpay()->simulateIssue($refMandate, $montant);
 
 
 ?>
