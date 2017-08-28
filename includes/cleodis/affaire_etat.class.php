aa4488f617a0ea3be39eddcf8c3bb7595a275a93
Morgan <mfleurquin@absystech.fr>
2017-08-28 10:18:30 +0200

    Affaire etat Cleodis

---
+++ includes/cleodis/affaire_etat.class.php
@@ -0,0 +1,28 @@
+<?
+/** Classe affaire_etat
+* @package Optima
+* @subpackage Cléodis
+*/
+class affaire_etat extends classes_optima {
+	public function __construct() {
+		parent::__construct();
+		$this->table = __CLASS__;
+		$this->controlled_by = 'affaire';
+		$this->colonnes["fields_column"] = array(
+			 'affaire_etat.date'
+			,"affaire_etat.id_affaire"
+			,"affaire_etat.etat"
+			,"affaire_etat.commentaire"
+			,'fichier_joint'=>array("custom"=>true,"nosort"=>true,"type"=>"file","width"=>50,"align"=>"center")
+		);
+		$this->files["fichier_joint"] = array("type"=>"pdf","preview"=>false,"no_upload"=>true,"no_generate"=>true);
+		$this->fieldstructure();
+	}
+
+
+	public function _GET($get, $post){
+		ATF::affaire_etat()->q->reset()->where("id_affaire", $get["id_affaire"])
+									   ->addOrder("date","desc");
+		return ATF::affaire_etat()->select_all();
+	}
+}
