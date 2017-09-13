44c34fde21ab27e48ebe9c0e11f3176f94e68fcc
Morgan <mfleurquin@absystech.fr>
2017-09-13 17:30:02 +0200

    LMA 15983 - Gestion de droit sur les taches (Pouvoir valider une tache qui nous concerne meme si pas le droit d'update de tache

--- includes/lm/tache.class.php
+++ includes/lm/tache.class.php
@@ -8,6 +8,8 @@ class tache_lm extends tache {
 	function __construct() {
 		$this->table = "tache";
 		parent::__construct();
+
+		$this->addPrivilege("valid");
 	}
 
 
@@ -26,6 +28,42 @@ class tache_lm extends tache {
 			}
 		}
 		return parent::default_value($field);
+	}
+
+	/**
+    * Valide une tache
+    * @author Nicolas BERTEMONT <nbertemont@absystech.fr>
+	* @param array $infos pour la validation
+	* @param array &$s La session
+	* @param array $files $_FILES
+	* @param array $cadre_refreshed Eventuellement des cadres HTML div à rafraichir...
+    */
+	public function valid($infos,&$s,$files=NULL,&$cadre_refreshed){
+		if (!$infos['id_tache']) return false;
+
+		$infos['etat'] = "fini";
+		$infos['complete'] = 100;
+		$infos['date_validation'] = date("Y-m-d H:i");
+		$infos['id_aboutisseur'] = ATF::$usr->getID();
+
+		$concerne = false;
+
+		ATF::tache_user()->q->reset()->where("id_tache", $this->decryptId($infos["id_tache"]),"AND")
+									 ->where("id_user",ATF::$usr->getID());
+		$concerne = ATF::tache_user()->select_all();
+
+
+		if ($concerne || ATF::$usr->privilege('tache','update')) {
+			if (parent::update($infos)) {
+				if ($email_envoye=$this->envoyer_mail($infos["id_tache"],"tache_valid")) {
+					ATF::$msg->addNotice(ATF::$usr->trans("email_envoye"));
+				}
+			}
+			return $infos['id_tache'];
+		}else{
+			throw new errorATF("Vous n'avez pas le droit de modifier cette tache");
+		}
+
 	}
 
 };
