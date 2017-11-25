a3422d172ed7c3c9987b8c605d3db1ce93afaa79
Quentin JANON <qjanon@absystech.fr>
2017-11-25 07:51:34 +0100

    FOnction de re ordonnancement des lignes periodique des contrat d'echeancier Ajout du tri pour l'affichage des lignes SQL pour la colonne offset

---
+++ sql/absystech-att.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `echeancier_ligne_periodique` ADD `offset` MEDIUMINT UNSIGNED NULL DEFAULT NULL AFTER `ref`;
+ALTER TABLE `echeancier_ligne_ponctuelle` ADD `offset` MEDIUMINT UNSIGNED NULL DEFAULT NULL AFTER `ref`;
