d848da39c0953a00667ba08873c447c3b135deb8
Quentin JANON <qjanon@absystech.fr>
2017-11-27 11:21:49 +0100

    SQL

--- sql/absystech-att.sql
+++ sql/absystech-att.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `echeancier_ligne_periodique` ADD `offset` MEDIUMINT UNSIGNED NULL DEFAULT NULL AFTER `ref`;
 ALTER TABLE `echeancier_ligne_ponctuelle` ADD `offset` MEDIUMINT UNSIGNED NULL DEFAULT NULL AFTER `ref`;
+ALTER TABLE `echeancier_ligne_periodique` CHANGE `offset` `offset` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '99999';
+ALTER TABLE `echeancier_ligne_ponctuelle` CHANGE `offset` `offset` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '99999';
