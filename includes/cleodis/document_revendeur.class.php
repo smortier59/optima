a591370e0fa605fca59740464ef7d6053f6d0b41
Fleurquin Morgan <mfleurquin@absystech.fr>
2018-01-16 11:45:12 +0100

    Field Nom document revendeur

--- includes/cleodis/document_revendeur.class.php
+++ includes/cleodis/document_revendeur.class.php
@@ -7,12 +7,15 @@ class document_revendeur extends classes_optima {
 		parent::__construct();
 		$this->table = __CLASS__;
 		$this->colonnes["fields_column"] = array(
-			'document_revendeur.id_societe'
+			 'document_revendeur.id_societe'
 			,'document_revendeur.site_associe'
 			,'fichier_joint'=>array("custom"=>true,"nosort"=>true,"type"=>"file","align"=>"center","renderer"=>"uploadFile")
 		);
 
 		$this->files["fichier_joint"] = array("type"=>"pdf","no_generate"=>true);
+
+		$this->field_nom= "%id_societe% - %site_associe% ";
+
 
 		$this->fieldstructure();
 	}
