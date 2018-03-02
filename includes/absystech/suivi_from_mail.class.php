6b201a914be7fddc1f39ee5a5a328f05e29441d5
Fleurquin Morgan <mfleurquin@absystech.fr>
2018-03-02 15:19:30 +0100

    Delocage pour ne plus etre bloqué pour la création de suivi si la boite mail ne répond pas

--- includes/absystech/suivi_from_mail.class.php
+++ includes/absystech/suivi_from_mail.class.php
@@ -11,23 +11,35 @@ class suivi_from_mail extends classes_optima {
 	 *
 	*/
 	public function recup_mail_suivi(){
+
 		$mail = 'optima.'.ATF::$codename.'.suivi@absystech.net';
-		$host = "zimbra.absystech.net";
+		$host = "zimbra3.absystech.net";
 		$port = 143;
 		$password = ATF::$codename=="att"?"P272nqX4tG":"43b52RaNNz";
 
-		ATF::imap()->init($host, $port, $mail, $password);
-		if (ATF::imap()->error) {	throw new errorATF(ATF::imap()->error); }
-		$mails = ATF::imap()->imap_fetch_overview('1:*');
+		log::logger($mail , "mfleurquin");
+		log::logger($host , "mfleurquin");
+		log::logger($port , "mfleurquin");
+		log::logger($password , "mfleurquin");
 
-		if (is_array($mails)){
-			foreach ($mails as $val) {
-				if($val->seen != 1){
-					$options[] = array($val->uid, $val->subject." | ".$val->from);
+		$options = array();
+
+		ATF::imap()->init($host, $port, $mail, $password);
+		if (ATF::imap()->error) {	/*throw new errorATF(ATF::imap()->error);*/
+			log::logger(ATF::imap()->error , "mfleurquin");
+		}else{
+			$mails = ATF::imap()->imap_fetch_overview('1:*');
+
+			if (is_array($mails)){
+				foreach ($mails as $val) {
+					if($val->seen != 1){
+						$options[] = array($val->uid, $val->subject." | ".$val->from);
+					}
 				}
 			}
+			ATF::imap()->imap_expunge();
 		}
-		ATF::imap()->imap_expunge();
+
 		return $options;
 	}
 }
