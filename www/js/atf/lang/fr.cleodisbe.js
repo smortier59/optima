cb45ebf860576d601d056011b7a6023e64d4c4ed
optima bore <optima@absytech.fr>
2018-03-06 16:14:47 +0100

    Commit language

--- www/js/atf/lang/fr.cleodisbe.js
+++ www/js/atf/lang/fr.cleodisbe.js
@@ -1,1 +1,1 @@
-jQuery.extend(ATF.loc,{"affaire_commentaire_facture3":"Commentaire de facture (3)","affaire_commentaire_facture2":"Commentaire de facture (2)","affaire_commentaire_facture":"Commentaire de facture","affaire_langue":"Langue"});
+jQuery.extend(ATF.loc,{"societe_particulier_civilite":"Civilité"});
