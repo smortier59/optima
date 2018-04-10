7781f8a389869250d4980367064dbfb28f641978
Fleurquin Morgan <mfleurquin@absystech.fr>
2018-04-10 17:10:07 +0200

    Revert "Ajout du type de pack Enki"
    This reverts commit da415022e5fa81150a1d340b79b0502c5fd75ab6.

--- sql/lm.sql
+++ sql/lm.sql
@@ -1,6 +1,3 @@
-ALTER TABLE `pack_produit` CHANGE `type_pack_magasin` `type_pack_magasin` ENUM('chaudiere','adoucisseur','alarme','enki') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-
-
 
 INSERT INTO `constante` (`id_constante`, `constante`, `valeur`)
 VALUES (NULL, '__FTP_SERVER__', '212.83.139.254'), (NULL, '__FTP_USERNAME__', 'cyrilPierreKO'), (NULL, '__FTP_USERPASS__', '8fSiD!wFm');
