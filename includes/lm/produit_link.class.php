74d9826bcc48e3e90673100fddfdc385c8d781d9
Yann GAUTHERON <ygautheron@absystech.fr>
2018-05-11 16:58:24 +0200

    Produits liés

---
+++ includes/lm/produit_link.class.php
@@ -0,0 +1,35 @@
+<?	
+/** Classe produit_link
+* @package Optima
+* @subpackage Cleodis
+*/
+class produit_link extends classes_optima {
+	function __construct() {
+		parent::__construct();
+		$this->table = __CLASS__;
+		$this->colonnes['fields_column'] = array( 
+			,'produit_link.id_produit'
+			,'produit_link.id_produit_cible'
+			,'produit_link.etat'
+		);
+
+		$this->colonnes['primary'] = array(
+			,"id_produit"
+			,'id_produit_cible'
+			,'etat'
+		);
+		
+		$this->colonnes['bloquees']['insert'] =  array('id_produit');
+		$this->colonnes['ligne'] =  array( 	
+			'produit_link.id_produit'
+			,'produit_link.id_produit_cible'
+			,'produit_link.etat'
+		);
+		
+		$this->field_nom = "%id_produit% %id_produit_cible% %etat%";
+		$this->foreign_key['id_produit_cible'] =  "produit";
+
+		$this->fieldstructure();	
+		$this->selectAllExtjs=true; 
+	}
+}
