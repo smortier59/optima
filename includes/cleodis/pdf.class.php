a7b7951d3b5a0094521c699ca626c55bc7e95211
Quentin JANON <qjanon@absystech.fr>
2018-07-03 16:42:55 +0200

    Plus de RUM / ICS

--- includes/cleodis/pdf.class.php
+++ includes/cleodis/pdf.class.php
@@ -5663,10 +5663,6 @@ class pdf_cleodis extends pdf {
 		}else{
 			$this->cell(0,5,"Par remboursement ou compensation",0,1);
 		}
-		if(ATF::$codename !== "cleodisbe"){
-			$this->cell(0,5,"RUM ".$this->affaire["RUM"],0,1);
-			$this->cell(0,5,"ICS ".__ICS__ ,0,1);
-		}
 
 		if($this->facture["mode_paiement"] == "virement" || $this->facture['mode_paiement'] =="mandat"){
 			$cadre = array();
