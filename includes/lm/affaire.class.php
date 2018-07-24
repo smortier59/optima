f5c17dfbf9430ed2f5fe4ecb32adf15f6b8a25c1
Fleurquin Morgan <mfleurquin@absystech.fr>
2018-07-24 11:02:00 +0200

    Modif opteven live

--- includes/lm/affaire.class.php
+++ includes/lm/affaire.class.php
@@ -1325,9 +1325,9 @@ class affaire_lm extends affaire {
 			if($comite){
 
 				ATF::commande()->q->reset()->where("commande.id_affaire", $va["id_affaire"], 'AND')
-										   ->where("commande.date_evolution", date("Y-m-d"), 'OR', 'contratPendingEncours', '>=')
-										   ->whereIsNull("commande.date_evolution", 'OR', 'contratPendingEncours')
 										   ->where("commande.etat", "arreter", 'AND', 'commandeEtat', "!=")
+										   ->where("commande.etat", "vente", 'AND', 'commandeEtat', "!=")
+										   ->where("commande.etat", "pending", 'AND', 'commandeEtat', "!=")
 										   ->where("commande.etat", "abandon", 'AND', 'commandeEtat', "!=");
 				$contrat = ATF::commande()->select_row();
 
