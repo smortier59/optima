6a389f0baed291783cd374fd2899c2386a503c32
Fleurquin Morgan <mfleurquin@absystech.fr>
2018-09-18 10:36:15 +0200

    batch ref societe vide

--- crontab/lm/batch_ref_societe.php
+++ crontab/lm/batch_ref_societe.php
@@ -8,24 +8,32 @@ ATF::define("tracabilite",false);
 $q = "SELECT id_societe, ref, date  FROM `societe` WHERE `ref` IS NULL";
 $client_sans_ref = ATF::db()->sql2array($q);
 
+ATF::begin_transaction();
+try{
+	foreach ($client_sans_ref as $key => $value){
 
-foreach ($client_sans_ref as $key => $value){
+		$ref = "SLI".date("Ym", strtotime($value["date"]));
 
-	$ref = "SLI".date("Ym", strtotime($value["date"]));
+		$max=ATF::societe()->get_max_ref($ref);
+		if($max<10){
+			$ref.='000'.$max;
+		}elseif($max<100){
+			$ref.='00'.$max;
+		}elseif($max<1000){
+			$ref.='0'.$max;
+		}elseif($max<10000){
+			$ref.=$max;
+		}else{
+			throw new errorATF(ATF::$usr->trans('ref_too_high'),80853);
+		}
 
-	$max=ATF::societe()->get_max_ref($ref);
-	if($max<10){
-		$ref.='000'.$max;
-	}elseif($max<100){
-		$ref.='00'.$max;
-	}elseif($max<1000){
-		$ref.='0'.$max;
-	}elseif($max<10000){
-		$ref.=$max;
-	}else{
-		throw new errorATF(ATF::$usr->trans('ref_too_high'),80853);
+		ATF::societe()->u->array("id_societe"=>$value["id_societe"], "ref"=>$ref);
+
+		echo $value["id_societe"]." ---> ".$ref."\n";
 	}
-
-	echo $value["id_societe"]." ---> ".$ref."\n";
-
+}catch(errorATF $e){
+	echo $e->getMessage();
+	ATF::rollback_transaction();
 }
+
+ATF::rollback_transaction();
