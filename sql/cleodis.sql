b946605006ff027096acf0e9d474fed25608bb2b
Quentin JANON <qjanon@absystech.fr>
2019-01-07 12:20:38 +0100

    Sauvegarde de champs supplémentaire sur les devis / commande lignes + le snapshot du pack produit sur l'affaire.

--- sql/cleodis.sql
+++ sql/cleodis.sql
@@ -29,3 +29,21 @@ ALTER TABLE `commande_ligne` ADD `duree` INT(11) NULL DEFAULT NULL,
 
 ALTER TABLE `devis_ligne` ADD `pack_produit` VARCHAR(255) NULL DEFAULT NULL AFTER `id_sous_categorie`, ADD `sous_categorie` VARCHAR(255) NULL DEFAULT NULL AFTER `pack_produit`;
 ALTER TABLE `commande_ligne` ADD `pack_produit` VARCHAR(255) NULL DEFAULT NULL AFTER `id_sous_categorie`, ADD `sous_categorie` VARCHAR(255) NULL DEFAULT NULL AFTER `pack_produit`;
+
+ALTER TABLE `devis_ligne` ADD `id_categorie` MEDIUMINT(8) UNSIGNED NULL DEFAULT NULL AFTER `sous_categorie`, 
+	ADD `categorie` VARCHAR(64) NULL DEFAULT NULL AFTER `id_categorie`, 
+	ADD `commentaire_produit` VARCHAR(512) NULL DEFAULT NULL AFTER `categorie`, 
+	ADD `visible_sur_site` ENUM('oui','non') NOT NULL DEFAULT 'non' AFTER `commentaire_produit`, 
+	ADD `visible_pdf` ENUM('oui','non') NOT NULL DEFAULT 'oui' AFTER `visible_sur_site`, 
+	ADD `ordre` INT NOT NULL DEFAULT '1' AFTER `visible_pdf`, 
+	ADD INDEX (`id_categorie`);
+
+ALTER TABLE `commande_ligne` ADD `id_categorie` MEDIUMINT(8) UNSIGNED NULL DEFAULT NULL AFTER `sous_categorie`, 
+	ADD `categorie` VARCHAR(64) NULL DEFAULT NULL AFTER `id_categorie`, 
+	ADD `commentaire_produit` VARCHAR(512) NULL DEFAULT NULL AFTER `categorie`, 
+	ADD `visible_sur_site` ENUM('oui','non') NOT NULL DEFAULT 'non' AFTER `commentaire_produit`, 
+	ADD `visible_pdf` ENUM('oui','non') NOT NULL DEFAULT 'oui' AFTER `visible_sur_site`, 
+	ADD `ordre` INT NOT NULL DEFAULT '1' AFTER `visible_pdf`, 
+	ADD INDEX (`id_categorie`);
+
+ALTER TABLE `affaire` ADD `snapshot_pack_produit` TEXT NULL DEFAULT NULL AFTER `hash_panier`;
