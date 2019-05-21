efc69cbea1168f039948fc216f103fed93e4eccc
Yan Philippe <yphilippe@absystech.fr>
2019-05-21 15:00:07 +0200

    Import
    Relative import

--- www/csv_files_folder/import_produit_catalogue_v2.php
+++ www/csv_files_folder/import_produit_catalogue_v2.php
@@ -2,5 +2,5 @@
 ini_set('display_errors', 1);
 ini_set("display_errors", 1);
 
-include("../../crontab/cleodis/boulangerpro/import_boulpro.php");
+include(dirname(__FILE__) ."/crontab/cleodis/boulangerpro/import_boulpro.php");
 ?>
