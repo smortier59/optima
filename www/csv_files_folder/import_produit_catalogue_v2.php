40aca841a39cb60366274dfb390237d57f8320c1
Yan Philippe <yphilippe@absystech.fr>
2019-05-21 15:58:32 +0200

    Fixes
    DIRNAME fixes + duplicate errors line ouput removed

--- www/csv_files_folder/import_produit_catalogue_v2.php
+++ www/csv_files_folder/import_produit_catalogue_v2.php
@@ -1,6 +1,5 @@
 <?
 ini_set('display_errors', 1);
-ini_set("display_errors", 1);
 
-include(dirname(__FILE__) ."/crontab/cleodis/boulangerpro/import_boulpro.php");
+include(dirname(__DIR__) ."../../crontab/cleodis/boulangerpro/import_boulpro.php");
 ?>
