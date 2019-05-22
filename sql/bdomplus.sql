869b795516010a0d66588f57fa76e3ca94ec8498
Fleurquin Morgan <mfleurquin@absystech.fr>
2019-05-22 10:48:02 +0200

    Update Magasin + facture magasin

--- sql/bdomplus.sql
+++ sql/bdomplus.sql
@@ -12,8 +12,13 @@ ALTER TABLE `facture_non_parvenue` CHANGE `ref` `ref` VARCHAR(36) CHARACTER SET 
 ALTER TABLE `facture` ADD `ref_magasin` VARCHAR(32) NULL AFTER `ref_externe`;
 
 
-CREATE TABLE `dev_optima_bdomplus`.`facture_magasin` ( `id_facture_magasin` MEDIUMINT NOT NULL AUTO_INCREMENT , `ref_facture` VARCHAR(25) NOT NULL , `id_affaire` MEDIUMINT NOT NULL , PRIMARY KEY (`id_facture_magasin`), INDEX (`id_affaire`)) ENGINE = InnoDB;
+CREATE TABLE facture_magasin ( `id_facture_magasin` MEDIUMINT NOT NULL AUTO_INCREMENT , `ref_facture` VARCHAR(25) NOT NULL , `id_affaire` MEDIUMINT NOT NULL , PRIMARY KEY (`id_facture_magasin`), INDEX (`id_affaire`)) ENGINE = InnoDB;
 ALTER TABLE `facture_magasin` CHANGE `id_affaire` `id_affaire` MEDIUMINT(9) UNSIGNED NOT NULL;
 ALTER TABLE `facture_magasin` CHANGE `id_facture_magasin` `id_facture_magasin` MEDIUMINT(9) UNSIGNED NOT NULL AUTO_INCREMENT;
 ALTER TABLE `facture_magasin` ADD FOREIGN KEY (`id_affaire`) REFERENCES `affaire`(`id_affaire`) ON DELETE RESTRICT ON UPDATE CASCADE;
 ALTER TABLE `facture_magasin` ADD `etat` ENUM('non_recu','paye') NOT NULL DEFAULT 'non_recu' AFTER `id_affaire`;
+
+ALTER TABLE `magasin` ADD `statut` ENUM('ouvert','ferme') NOT NULL DEFAULT 'ouvert' AFTER `id_societe`;
+
+
+
