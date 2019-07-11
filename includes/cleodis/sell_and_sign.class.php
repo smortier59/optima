f724a22d71585611bf5acc2fd23eef65feefac48
Quentin JANON <qjanon@absystech.fr>
2019-07-11 15:11:23 +0200

    Merge branch 'bdomplus/hotfix/alert_stock_licence' into 'bdomplus/lot-2'
    Cron de controle de stock de licence
    
    See merge request absystech/absystech.fr-optima!37

--- includes/cleodis/sell_and_sign.class.php
+++ includes/cleodis/sell_and_sign.class.php
@@ -12,6 +12,19 @@ class sell_and_sign extends classes_optima {
 		parent::__construct();
 		$this->controlled_by = 'affaire';
 		$this->table = "sell_and_sign";
+		$this->colonnes['primary'] = array(
+			"sell_and_sign"
+			,"date"
+			,"id_affaire"
+			,"dataCustomer_number"
+			,"contractor_id"
+			,"contract_id"
+			,"document_id"
+			,"contractorTo_id"
+			,"bundle_id"
+		);
+		$this->fieldstructure();
+		$this->files["dossier_preuve"] = array("type"=>"zip","preview"=>false,"no_upload"=>false,"no_generate"=>true);
 	}	
 };
 
