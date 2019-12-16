2f007e3b739975093d214eb7c0a291f104374dfd
Fleurquin Morgan <mfleurquin@absystech.fr>
2019-12-16 16:05:26 +0100

    Mise en place des types d'affaire

--- sql/cleodis.sql
+++ sql/cleodis.sql
@@ -1,1 +1,16 @@
-ALTER TABLE `affaire` CHANGE `type_affaire` `type_affaire` ENUM('normal','2SI','Olisys') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'normal';
+ALTER TABLE `affaire` CHANGE `type_affaire` `type_affaire` ENUM('normal','2SI','Boulanger Pro',
+'Consommables_com',
+'DIB',
+'Dyadem',
+'FLEXFUEL',
+'Instore',
+'LAFI',
+'Manganelli',
+'NRC',
+'OLISYS - Ma Solution IT',
+'Proxi Pause',
+'Trekk',
+'ZENCONNECT – ZEN PACK') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'normal';
+
+
+
