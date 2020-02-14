799e2bb8e447e40ebc375138af55079326b76df7
Quentin JANON <qjanon@absystech.fr>
2020-02-14 11:56:33 +0100

    Ajsutement condition pour attaquer les ID, bah vi, ça sert a rien de régénérer les factures si la TVA reste a 1

--- crontab/bdomplus/regenerate-facture.php
+++ crontab/bdomplus/regenerate-facture.php
@@ -13,11 +13,66 @@ ATF::facture()->q->reset();
 // Uniquement les avoirs
 ATF::facture()->q->where("prix", "0", "OR", 'k1', "<");
 // Facture avec un taux de TVA a 1
-ATF::facture()->q->where("tva", "1.000", "OR", 'k1');
+// ATF::facture()->q->where("tva", "1.000", "OR", 'k1');
+ATF::facture()->q->where("id_facture",'2125',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2127',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2128',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2129',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2130',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2131',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2132',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2133',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2134',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2135',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2136',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2137',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2138',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2139',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2140',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2141',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2142',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2143',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2144',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2145',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2146',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2147',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2148',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2149',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2150',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2151',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2152',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2153',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2154',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2155',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2156',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2157',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2158',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2159',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2160',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2161',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2162',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2163',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2164',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2165',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2166',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2167',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2168',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2193',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2194',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2195',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2196',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2197',"OR","k1","=");
+ATF::facture()->q->where("id_facture",'2198',"OR","k1","=");
+
+
 ATF::facture()->q->setToString();
 log::logger(ATF::facture()->sa(), $logFile);
+echo ATF::facture()->sa()."\n";
 ATF::facture()->q->unsetToString();
 $factures = ATF::facture()->sa();
+
+
+
 
 log::logger("====== Nombre de factures à traiter : ".count($factures), $logFile);
 
