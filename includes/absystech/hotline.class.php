e82377c514340578c6e9400a79e83b988d03d6f0
jdelaporte <jdelaporte@absystech.fr>
2020-02-26 14:39:54 +0100

    Air France autorisation mauriciens

--- includes/absystech/hotline.class.php
+++ includes/absystech/hotline.class.php
@@ -3883,6 +3883,7 @@ if (!ATF::isTestUnitaire()) $result = `$cmd`;
 		// Profil développeur extérieur (PATCH DEGUEU EN MODE BRICOLE)
 		if (ATF::$usr->get('id_profil') == 16) {
 			$this->q->where("gep_projet.id_societe", 513); // Cléodis
+			$this->q->where("gep_projet.id_gep_projet", 202); // AirFrance
 		}
 
 		// $this->q->setToString();
