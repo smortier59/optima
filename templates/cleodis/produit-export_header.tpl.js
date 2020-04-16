02d95ecfc0d6ad5b20ee3a8caa7d4bd9fc6e21a2
Quentin JANON <qjanon@absystech.fr>
2020-04-16 17:47:27 +0200

    :sparkles: Export des produits a la mode BoulPro Ajout du bouton d'export dans le header du listing des produits Création de l'export qui prend les 750 derniers packs avec ses lignes et ses produits pour les transpiler en excel

---
+++ templates/cleodis/produit-export_header.tpl.js
@@ -0,0 +1,8 @@
+{strip}
+{
+	text: '{ATF::$usr->trans(export_middleware,$current_class->name())}'
+	, handler: function(b,e){
+		window.location='{$current_class->name()},export_middleware.ajax,onglet={$pager}';
+	}
+},
+{/strip}
