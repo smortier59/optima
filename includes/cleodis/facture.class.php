325c84d692ca1e0e8cba0a867ddddae0ac3755db
Fleurquin Morgan <mfleurquin@absystech.fr>
2020-09-02 16:39:04 +0200

    Correctif si date rejet ou regul

--- includes/cleodis/facture.class.php
+++ includes/cleodis/facture.class.php
@@ -833,6 +833,9 @@ class facture_cleodis extends facture {
 				);
 			}
 
+			$commande = $this->select($infos["id_facture"] , "facture.id_commande");
+			ATF::commande()->checkEtatContentieux($commande);
+
 			log::logger("--> Appel Mauvais payeur" , "mauvais_payeur");
 			ATF::societe()->checkMauvaisPayeur($this->select($this->decryptId($infos["id_facture"]) , "id_societe"));
 
