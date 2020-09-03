6a39cef986506f366f074944d79de32f279cfc08
Quentin JANON <qjanon@absystech.fr>
2020-09-03 17:36:33 +0200

    Ajuste cleodis et non cleodis be

--- crontab/cleodis/regenerate_facture.php
+++ crontab/cleodis/regenerate_facture.php
@@ -1,16 +1,17 @@
 <?php
 define("__BYPASS__",true);
-$_SERVER["argv"][1] = "cleodisbe";
+$_SERVER["argv"][1] = "cleodis";
 include(dirname(__FILE__)."/../../global.inc.php");
 ATF::define("tracabilite",false);
 
 $dataPath = __DATA_PATH__."cleodis/";
 
-ATF::facture()->q->reset()->where("facture.date","2020-10-01");
+ATF::facture()->q->reset()->addAllFields('facture')->where("facture.date","2020-10-01");
 $factures = ATF::facture()->select_all();
 
-//echo count($factures);
+echo count($factures);
+
 foreach ($factures as $key => $value) {
-	ATF::facture()->move_files($value["facture.id_facture"]);
+  ATF::facture()->move_files($value["facture.id_facture"]);
 }
 ?>
