2bef16d02a8d1fb91738e412a934891ec802c6d2
Fleurquin Morgan <mfleurquin@absystech.fr>
2020-10-05 14:17:37 +0200

    CheckDomaine Email en doublon

--- includes/cleodis/souscription.class.php
+++ includes/cleodis/souscription.class.php
@@ -158,6 +158,7 @@ class souscription_cleodis extends souscription {
         $libelle = $this->getLibelleAffaire($post['id_pack_produit'], $post['site_associe'], $post["renouvellement"]);
 
 
+
         $id_devis = $this->createDevis($post, $libelle);
 
         ATF::devis()->q->reset()->addField('devis.id_affaire','id_affaire')->where('devis.id_devis', $id_devis);
@@ -421,7 +422,7 @@ class souscription_cleodis extends souscription {
 
 
     foreach ($produits as $k=>$produit) {
-        
+
         ATF::produit()->q->reset()
           ->addField("loyer")
           ->addField("duree")
@@ -431,10 +432,10 @@ class souscription_cleodis extends souscription {
           ->addField("prix_achat")
           ->addField("id_fournisseur")
           ->where("id_produit", $produit['id_produit']);
-          
+
         $produitLoyer = ATF::produit()->select_row();
 
-         
+
         if ($produit['id_pack_produit']) {
           $id_pack = $produit['id_pack_produit'];
 
@@ -1167,19 +1168,6 @@ class souscription_cleodis extends souscription {
       );
       ATF::devis()->perdu($infos);
     }
-  }
-
-  /**
-   * Appel la fonction pour vérifier la validité du domaine d'un email
-   * @author Quentin JANON <qjanon@absystech.fr>
-   */
-  public function _checkDomainEmail($get) {
-    try {
-      mail::check_mail($get['email']);
-    } catch (errorATF $e) {
-      throw new errorATF("INVALID DOMAIN",500);
-    }
-    return true;
   }
 
 
