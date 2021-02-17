815f1416bea27f1ad011f9f1dee4ac417875d6a9
Fleurquin Morgan <mfleurquin@absystech.fr>
2021-02-17 12:23:16 +0100

    Batch espace client

--- crontab/bdomplus/batch_compte_client_ecp.php
+++ crontab/bdomplus/batch_compte_client_ecp.php
@@ -10,156 +10,105 @@ if(!$_SERVER["argv"][2] || ($_SERVER["argv"][2] != "list" && $_SERVER["argv"][2]
 }
 $type = $_SERVER["argv"][2];
 
-ATF::constante()->q->reset()->where("constante", "__URL_ESPACE_CLIENT__");
-$url_front_espace_client = ATF::constante()->select_row();
-if(!$url_front_espace_client){
-    echo "Il n'y a pas de constante __URL_ESPACE_CLIENT__ pour l'url de l'espace client\n";
-    return;
-}
-$url_front_espace_client = $url_front_espace_client["valeur"];
-
-ATF::constante()->q->reset()->where("constante", "__URL_ESPACE_CLIENT_BACK__");
-$url_back_espace_client = ATF::constante()->select_row();
-if (!$url_back_espace_client) {
-    echo "Il n'y a pas de constante __URL_ESPACE_CLIENT_BACK__ pour l'url de l'espace client\n";
-    return;
+try{
+    $url_back_espace_client = ATF::espace_client_conseiller()->getUrlBack();
+    $url_front_espace_client = ATF::espace_client_conseiller()->getUrlFront();
+}catch(errorATF $e){
+    echo "Une erreur est survenue : \n\n";
+    echo $e->getMessage();
 }
 
-
-$url_back_espace_client = $url_back_espace_client["valeur"];
-$application = getApplicationByDomain($url_back_espace_client, $url_front_espace_client);
-if(!$application) {
-    echo "Application ID non récupérée\n";
-    return;
-}else{
-    $application = json_decode($application);
-    if(!$application->_id){
-        echo "Application ID récupéré incorrect\n";
-        echo "---". $application."\n";
+if($url_back_espace_client &&  $url_front_espace_client){
+    $application = ATF::espace_client_conseiller()->getApplicationByDomain($url_back_espace_client, $url_front_espace_client);
+    if(!$application) {
+        echo "Application ID non récupérée\n";
         return;
-    }
-}
-
-    // $mongoConnection = new MongoClient("mongodb://172.16.255.211:27017");
-
-    echo "Application ID --> ".$app->_id."\n";
-
-echo "Tout est OK, on commence le taff \n";
-
-//Recupere toute les affaires en cours donc on a pas encore envoyé de mail de création de compte au client
-//Pour chaque affaire, on recupere le client
-$q =   "SELECT commande.ref AS ref_contrat,
-             commande.id_societe AS id_societe,
-             societe.particulier_nom AS nom_client,
-             societe.particulier_prenom AS prenom_client,
-             societe.particulier_email AS email_client,
-             commande.id_affaire AS id_affaire
-        FROM commande
-        LEFT JOIN societe ON commande.id_societe = societe.id_societe
-        WHERE commande.etat != 'arreter'
-        AND commande.etat != 'arreter_contentieux'
-        AND commande.retour_contrat IS NOT NULL
-        AND societe.date_envoi_mail_creation_compte IS NULL";
-
-
-
-$contratsEnCours = ATF::db()->sql2array($q);
-//log::logger($contratsEnCours, "mfleurquin");
-
-$clientSansCompte = array();
-
-foreach ($contratsEnCours as $key => $value) {
-
-    $client = array("id_societe" => $value["id_societe"],
-                    "nom" => $value["nom_client"],
-                    "prenom" => $value["prenom_client"],
-                    "email" => $value["email_client"],
-                    "ref" => $value["ref_contrat"],
-                    "affaire" => $value["id_affaire"]
-                );
-
-    //Pour chaque client, on check si on compte existe sur ECC
-    //$accountExist = accountExisteEcc($url_back_espace_client, $client["email"], $application->_id );
-
-    $clientSansCompte[] = $client;
-}
-
-//log::logger($clientSansCompte , "mfleurquin");
-
-
-//Si list, on envoi la liste des client à Benjamin
-if($type === "list") {
-    $mail = new mail(
-        array(
-            "recipient" => "benjamin.tronquit@cleodis.com",
-            "objet" => "Client sans compte",
-            "template" => "listing_client_sans_compte",
-            "clients" => $clientSansCompte
-        )
-    );
-    $mail->send();
-}
-//Si envoi_client, on envoi un mail à chque client avec lien pour créer son compte
-if($type === "envoi_client") {
-    $i=0;
-    foreach ($clientSansCompte as $k => $client) {
-        if($i < 2){
-            ATF::societe()->demande_creation_compte_espace_client($client, $url_front_espace_client);
-            $i++;
+    }else{
+        $application = json_decode($application);
+        if(!$application->_id){
+            echo "Application ID récupéré incorrect\n";
+            echo "---". $application."\n";
+            return;
         }
     }
+
+    echo "Application ID --> ".$app->_id."\n";
+    echo "Tout est OK, on commence le taff \n";
+
+    //Recupere toute les affaires en cours donc on a pas encore envoyé de mail de création de compte au client
+    //Pour chaque affaire, on recupere le client
+    $q =   "SELECT commande.ref AS ref_contrat,
+                commande.id_societe AS id_societe,
+                societe.particulier_nom AS nom_client,
+                societe.particulier_prenom AS prenom_client,
+                societe.particulier_email AS email_client,
+                commande.id_affaire AS id_affaire
+            FROM commande
+            LEFT JOIN societe ON commande.id_societe = societe.id_societe
+            WHERE commande.etat != 'arreter'
+            AND commande.etat != 'arreter_contentieux'
+            AND commande.retour_contrat IS NOT NULL
+            AND societe.date_envoi_mail_creation_compte IS NULL";
+
+
+
+    $contratsEnCours = ATF::db()->sql2array($q);
+
+    $clientSansCompte = array();
+    $clients = array();
+    $i = 0;
+    foreach ($contratsEnCours as $key => $value) {
+
+        $client = array("id_societe" => $value["id_societe"],
+                        "nom" => $value["nom_client"],
+                        "prenom" => $value["prenom_client"],
+                        "email" => $value["email_client"],
+                        "ref" => $value["ref_contrat"],
+                        "affaire" => $value["id_affaire"]
+                    );
+        $clients[] = $client;
+    }
+    $clients = ATF::espace_client_conseiller()->checkAccountsExiste($url_back_espace_client, $application->_id,  $clients );
+    $clients = json_decode($clients, true);
+
+
+    foreach ($clients["clients"] as $key => $value) {
+       if(!$value["existe"]){
+        $clientSansCompte[] = $value;
+       }else{
+        log::logger("Client existant -->", "mfleurquin");
+        log::logger($value, "mfleurquin");
+       }
+    }
+
+
+    //Si list, on envoi la liste des client à Benjamin
+    if($type === "list") {
+        echo "Envoi du mail contenant la liste des clients sans compte à benjamin.tronquit@cleodis.com\n";
+        $mail = new mail(
+            array(
+                "recipient" => "benjamin.tronquit@cleodis.com",
+                "objet" => "Client sans compte",
+                "template" => "listing_client_sans_compte",
+                "clients" => $clientSansCompte
+            )
+        );
+        $mail->send();
+    }
+    //Si envoi_client, on envoi un mail à chque client avec lien pour créer son compte
+    if($type === "envoi_client") {
+        foreach ($clientSansCompte as $k => $client) {
+            echo "Envoi du mail contenant la demande de création de compte à ".$client["email"]."\n";
+            ATF::societe()->demande_creation_compte_espace_client($client, $url_front_espace_client);
+
+
+        }
+    }
+
+
 }
 
 
-/*
-function getApplicationByDomain($url_back_espace_client, $url_front_espace_client) {
-    $curl = curl_init();
-    $host = str_replace("http://", "", $url_back_espace_client);
-    $host = str_replace("https://", "", $host);
 
-    echo "Récupération de l'application ID, appel CURL : ".$url_back_espace_client . '/application/domain?domain=' . $url_front_espace_client."\n";
 
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => $url_back_espace_client.'/application/domain?domain=' . $url_front_espace_client,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
-        CURLOPT_HTTPHEADER => array(
-            'host: '. $host
-        ),
-    ));
 
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-    return $response;
-}
-
-function accountExisteEcc($url_back_espace_client, $ecmailClient, $applicationId) {
-    $curl = curl_init();
-
-    echo "Check si le client à un compte : " . $url_back_espace_client . '/account/existAccountForOptima?email=' . $ecmailClient . '&applicationId=' . $applicationId."\n";
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => $url_back_espace_client.'/account/existAccountForOptima?email='. $ecmailClient .'&applicationId='. $applicationId,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: text/plain'
-        ),
-    ));
-
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-    return $response;
-}*/
