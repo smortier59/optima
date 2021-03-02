8addd6ef5abcb2684243c144c13be365b376ab15
Fleurquin Morgan <mfleurquin@absystech.fr>
2021-03-02 13:46:05 +0100

    Ajout d'une classe telescope ppur recuperer les infos telescope
    Theme d'un telescope + infos des telescopes

---
+++ includes/absystech/telescope.class.php
@@ -0,0 +1,46 @@
+<?
+/**
+* Classe telescope
+* @package Optima
+* @subpackage AbsysTech
+*/
+class telescope extends classes_optima {
+	/**
+	* Constructeur !
+	*/
+	public function __construct() {
+		parent::__construct();
+
+		$this->table = "telescope";
+		$this->colonnes['fields_column'] = array(
+			'telescope.telescope',
+			'telescope.codename',
+			'telescope.url',
+			'telescope.theme',
+			'telscope.actif'
+		);
+
+		$this->fieldstructure();
+
+	}
+
+	/**
+	 * [_getTelescopeInfos description]
+	 * @author : Morgan FLEURQUIN <mfleurquin@absystech.fr>
+	 * @param  Array $get
+	 *         			url -> URL du telescope
+	 * @return array    infos telescope
+	 */
+	public function _getTelescopeInfos($get) {
+
+		if(!$get["url"]) throw new errorATF("URL manquante");
+		ATF::telescope()->q->reset()->where("url", $get["url"]);
+		return ATF::telescope()->select_row();
+
+	}
+
+	public function _getTelescopesActif() {
+		ATF::telescope()->q->reset()->where("actif", "oui");
+		return ATF::telescope()->select_all();
+	}
+}
