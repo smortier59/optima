7d58ced4a573c0c262df566523ff4db21378f1de
diame sarr <dsarr@absystech.fr>
2021-04-12 14:16:21 +0200

    suite de developpement concernant le Lettrage des prelevement

--- includes/prelevement.class.php
+++ includes/prelevement.class.php
@@ -13,7 +13,7 @@ class prelevement extends classes_optima{
     public function _uploadAndParseXml($get, $post, $files){
         log::logger("HERE", "dsarr");
         try {
-            // $r = ATF::file()->_POST($get, $post, $files);
+            $r = ATF::file()->_POST($get, $post, $files);
 
             // Deuxième partie : Parsing du XML + renvoi d'un tableau exploitable avec les données
             $this->parseXML();
@@ -27,54 +27,43 @@ class prelevement extends classes_optima{
         $filepath = $this->filepath(ATF::$usr->getID(), "fichier_joint", true);
         log::logger($filepath, "dsarr");
 
-        $xmlData = simplexml_load_file($filepath);
+        $arr = array();
+
+        $xmlData = json_decode(json_encode(simplexml_load_file($filepath)),true);
+
+        if ( ! empty($xmlData)) {
+            $i=0;
+            foreach ($xmlData['CstmrDrctDbtInitn']['PmtInf']['DrctDbtTxInf'] as $elem) {
+              $arr[$i]['montant'] = $elem['InstdAmt'];
+              $arr[$i]['date_facture'] = $elem['DrctDbtTx']['MndtRltdInf']['DtOfSgntr'];
+              $arr[$i]['rum'] = $elem['DrctDbtTx']['MndtRltdInf']['MndtId'];
+              $arr[$i]['id_facture'] = $elem['PmtId']['InstrId'];
+              $arr[$i]['name_client'] = $elem['Dbtr']['Nm'];
+             ++$i;
+            }
+        }
+
         // log::logger($xmlData, "dsarr");
-        log::logger($xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->InstdAmt[0], "dsarr");
-        $montantTTC = $xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->InstdAmt[0];
-        $rum = $xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->MndtRltdInf->MndtId[0];
-        $date_facture = $xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->DrctDbtTx->MndtRltdInf->DtOfSgntr[0];
+        // log::logger($xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->InstdAmt[0], "dsarr");
+        // $montantTTC = $xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->InstdAmt[0];
+        // $rum = $xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->MndtRltdInf->MndtId[0];
+        // $date_facture = $xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->DrctDbtTx->MndtRltdInf->DtOfSgntr[0];
 
-        log::logger($rum,"dsarr");
-        log::logger($date_facture,"dsarr");
+        // log::logger($rum,"dsarr");
+        // log::logger($date_facture,"dsarr");
         
 
-        $retour = array();
 
-        // Il me faut : Date facture / Num Facture / RUM / Montant TTC / Ref Client / Nom Client
-
-// CstmrDrctDbtInitn > PmtInf > DrctDbtTxInf => Ligne de paiement
-// CstmrDrctDbtInitn > PmtInf > DrctDbtTxInf > InstdAmt => Montant TTC
-// CstmrDrctDbtInitn > PmtInf > DrctDbtTxInf > DrctDbtTx > MndtRltdInf > MndtId => RUM
-// CstmrDrctDbtInitn > PmtInf > ReqdColltnDt => Date de facture ?
-// CstmrDrctDbtInitn > PmtInf > DrctDbtTxInf > DrctDbtTx > MndtRltdInf > DtOfSgntr => Date de facture ?
-        
-    
-
-
-        // $retour = array(
-        //     array(
-        //         "date_facture"=>$date_facture,
-        //         "num_facture"=>$num_facture,
-        //         "rum"=>$rum,
-        //         ...
-        //     ),
-        //     array(
-        //         "date_facture"=>$date_facture,
-        //         "num_facture"=>$num_facture,
-        //         "rum"=>$rum,
-        //         ...
-        //     ),
-        //     array(
-        //         "date_facture"=>$date_facture,
-        //         "num_facture"=>$num_facture,
-        //         "rum"=>$rum,
-        //         ...
-        //     ),
-        // );
-
-        return $xmlData ;
+        return $arr ;
 
         // Traiter le fichier XML
+    }
+
+    public function _payments($get,$post){
+     if($post){
+         return true;
+     }
+
     }
 	
 };
