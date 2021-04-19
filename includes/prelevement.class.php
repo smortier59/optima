ddaef15e955f484dba5580d7c10b5a9ad1a72baf
diame sarr <dsarr@absystech.fr>
2021-04-19 10:17:13 +0200

    refacto total du code

--- includes/prelevement.class.php
+++ includes/prelevement.class.php
@@ -11,60 +11,77 @@ class prelevement extends classes_optima{
 	}
 
     public function _uploadAndParseXml($get, $post, $files){
-        log::logger("HERE", "dsarr");
         try {
+           $filepath = $this->filepath(ATF::$usr->getID(), "fichier_joint", true);
+           unlink($filepath);
             $r = ATF::file()->_POST($get, $post, $files);
-
-            // Deuxième partie : Parsing du XML + renvoi d'un tableau exploitable avec les données
-            $this->parseXML();
         } catch (Exception $e) {
             throw $e;
         }
     }
 
-    public function parseXML() {
-        log::logger('1ère méthode' , 'dsarr');
+    public function _parseXML() {
+	
         $filepath = $this->filepath(ATF::$usr->getID(), "fichier_joint", true);
-        log::logger($filepath, "dsarr");
 
-        $arr = array();
+        if(file_exists($filepath) && filesize($filepath) !=0){
+						$arr = array();
 
-        $xmlData = json_decode(json_encode(simplexml_load_file($filepath)),true);
+						$xmlData = json_decode(json_encode(simplexml_load_file($filepath)),true);
+            
+            if (count($xmlData) !=0 ) {
+                $i=0;
+                foreach ($xmlData['CstmrDrctDbtInitn']['PmtInf']['DrctDbtTxInf'] as $elem) {
+                  ATF::facture()->q->reset()->where("facture.ref", $elem['PmtId']['InstrId']);
+			            $facture = ATF::facture()->select_row();
 
-        if ( ! empty($xmlData)) {
-            $i=0;
-            foreach ($xmlData['CstmrDrctDbtInitn']['PmtInf']['DrctDbtTxInf'] as $elem) {
-              $arr[$i]['montant'] = $elem['InstdAmt'];
-              $arr[$i]['date_facture'] = $elem['DrctDbtTx']['MndtRltdInf']['DtOfSgntr'];
-              $arr[$i]['rum'] = $elem['DrctDbtTx']['MndtRltdInf']['MndtId'];
-              $arr[$i]['id_facture'] = $elem['PmtId']['InstrId'];
-              $arr[$i]['name_client'] = $elem['Dbtr']['Nm'];
-             ++$i;
-            }
+                  $id_societe = $facture["facture.id_societe_fk"];
+
+                  if(!empty($id_societe)){
+                    ATF::societe()->q->reset()->where("id_societe",$id_societe);
+                    $societe = ATF::societe()->select_row();
+                  }
+                  
+
+                  $ref_client = $societe["ref"];
+                  $name_societe = $societe["societe"];
+
+                  $arr[$i]['montant'] = $elem['InstdAmt'];
+                  $arr[$i]['date_facture'] = $elem['DrctDbtTx']['MndtRltdInf']['DtOfSgntr'];
+                  $arr[$i]['rum'] = $elem['DrctDbtTx']['MndtRltdInf']['MndtId'];
+                  $arr[$i]['id_facture'] = $elem['PmtId']['InstrId'];
+
+
+                  if($elem['PmtId']['InstrId'] == $facture["facture.ref"]){
+                    $arr[$i]['name_client'] = $name_societe;
+                    $arr[$i]['ref_client'] = $ref_client;
+                  }else{
+                    $arr[$i]['name_client'] = $elem['Dbtr']['Nm'];
+                  }
+                 
+                 ++$i;
+                }
+								return $arr ;
+            }else{
+              throw new errorATF('Votre fichier est vide', 500);
+						}
+        }else{
+					throw new errorATF("Le fichier n'existe pas", 500);
         }
 
-        // log::logger($xmlData, "dsarr");
-        // log::logger($xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->InstdAmt[0], "dsarr");
-        // $montantTTC = $xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->InstdAmt[0];
-        // $rum = $xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->MndtRltdInf->MndtId[0];
-        // $date_facture = $xmlData->CstmrDrctDbtInitn->PmtInf->DrctDbtTxInf->DrctDbtTx->MndtRltdInf->DtOfSgntr[0];
-
-        // log::logger($rum,"dsarr");
-        // log::logger($date_facture,"dsarr");
-        
-
-
-        return $arr ;
 
         // Traiter le fichier XML
     }
 
     public function _payments($get,$post){
      if($post){
-         return true;
+      return true;
      }
 
     }
+    
 	
 };
+
+
 ?>
