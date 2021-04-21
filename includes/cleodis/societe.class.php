64aee2eef143160702c214b2efdc3836e1da3703
Fleurquin Morgan <mfleurquin@absystech.fr>
2021-04-21 14:50:25 +0200

    Retrait de log

--- includes/cleodis/societe.class.php
+++ includes/cleodis/societe.class.php
@@ -2086,7 +2086,6 @@ class societe_cleodis extends societe {
         "colors" => $colors,
         "partenaire"=> $partenaire
       );
-      log::logger($infos_mail, "mfleurquin");
 
       $mail = new mail( $infos_mail );
       if($mail->send()){
