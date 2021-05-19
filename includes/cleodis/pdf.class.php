2b3b43c8c1727571e961ed448f693d66d3bb8efc
Quentin JANON <qjanon@absystech.fr>
2021-05-19 15:56:03 +0200

    Ajustement de la condition pour les PDF type affaire (correctif Location evolutive lorsqu'il n'y a pas de type affaire.)

--- includes/cleodis/pdf.class.php
+++ includes/cleodis/pdf.class.php
@@ -438,7 +438,7 @@ class pdf_cleodis extends pdf {
 				$this->devisVente();
 			}elseif($this->affaire['nature'] =='avenant'){
 				$this->devisAvenant();
-			}elseif(ATF::type_affaire()->select($this->affaire["id_type_affaire"], "devis_template") !== 'devis') {
+			}elseif($this->affaire["id_type_affaire"] && ATF::type_affaire()->select($this->affaire["id_type_affaire"], "devis_template") !== 'devis') {
 				$template = ATF::type_affaire()->select($this->affaire["id_type_affaire"], "devis_template");
 				$this->$template(); // On appel la fonction du template stocké en BDD
 			}else{
