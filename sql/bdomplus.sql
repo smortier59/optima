5a413dbef9e9a16de27f2d5d30dd5e485613f233
Fleurquin Morgan <mfleurquin@absystech.fr>
2021-10-22 08:54:39 +0200

    recette

---
+++ sql/bdomplus.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `affaire` ADD `id_commercial` MEDIUMINT(8) UNSIGNED NULL DEFAULT NULL AFTER `id_apporteur`, ADD INDEX (`id_commercial`);
+ALTER TABLE `affaire` ADD FOREIGN KEY (`id_commercial`) REFERENCES `user`(`id_user`) ON DELETE SET NULL ON UPDATE CASCADE;
