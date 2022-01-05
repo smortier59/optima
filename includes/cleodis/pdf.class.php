b8bd7610acd4cb9ef2fa4a5af5c1e6f9f7caa2ad
Fleurquin Morgan <mfleurquin@absystech.fr>
2022-01-05 09:16:49 +0100

    :pencil2: style: Affichage de la periode uniquement si redevence à oui pour le pdf de facture

--- includes/cleodis/pdf.class.php
+++ includes/cleodis/pdf.class.php
@@ -15875,7 +15875,12 @@ class pdf_go_abonnement extends pdf_cleodis {
 			$data[0][0] = "1";
 
 			$data[0][1] = "Redevance Abonnement";
-			$data[0][1] .= "\nPour la période du ".date("d/m/Y",strtotime($this->facture['date_periode_debut']))." au ".date("d/m/Y",strtotime($this->facture['date_periode_fin']));
+
+			if($this->facture["redevance"] === "oui"){
+				if($this->affaire['ref'] && $this->affaire['nature']!="vente"){
+					$data[0][1] .= "\nPour la période du ".date("d/m/Y",strtotime($this->facture['date_periode_debut']))." au ".date("d/m/Y",strtotime($this->facture['date_periode_fin']));
+				}
+			}
 			$data[0][1] .= "\nContrat n°".$this->affaire['ref']."-".$this->client["ref"].' - '.$this->affaire["affaire"];
 
 
