82515f86145c0405760cfc29d028e711d238128e
Morgan <mfleurquin@absystech.fr>
2022-02-22 15:48:36 +0100

    Gestion conflit

--- includes/cleodis/pdf.class.php
+++ includes/cleodis/pdf.class.php
@@ -15809,14 +15809,14 @@ class pdf_go_abonnement extends pdf_cleodis {
 			if ($this->facture['designation']) {
 				$data[0][1] = $this->facture['designation'];
 			} else {
-			$data[0][1] = "Redevance Abonnement";
+				$data[0][1] = "Redevance Abonnement";
 
-			if($this->facture["redevance"] === "oui"){
-				if($this->affaire['ref'] && $this->affaire['nature']!="vente"){
-					$data[0][1] .= "\nPour la période du ".date("d/m/Y",strtotime($this->facture['date_periode_debut']))." au ".date("d/m/Y",strtotime($this->facture['date_periode_fin']));
+				if($this->facture["redevance"] === "oui"){
+					if($this->affaire['ref'] && $this->affaire['nature']!="vente"){
+						$data[0][1] .= "\nPour la période du ".date("d/m/Y",strtotime($this->facture['date_periode_debut']))." au ".date("d/m/Y",strtotime($this->facture['date_periode_fin']));
+					}
 				}
-			}
-			$data[0][1] .= "\nContrat n°".$this->affaire['ref']."-".$this->client["ref"].' - '.$this->affaire["affaire"];
+				$data[0][1] .= "\nContrat n°".$this->affaire['ref']."-".$this->client["ref"].' - '.$this->affaire["affaire"];
 			}
 
 			//Désignation L3
