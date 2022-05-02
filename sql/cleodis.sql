3c015def809d153c5c57be07bfd5336349651773
Morgan <mfleurquin@absystech.fr>
2022-05-02 09:40:02 +0200

    Changement loyer_kilometrage en restitution

--- sql/cleodis.sql
+++ sql/cleodis.sql
@@ -47,7 +47,7 @@ left join `commande` on
 where
     `affaire`.`etat` not in ('demande_refi', 'facture_refi')
     and (`commande`.`etat` in ('mis_loyer', 'prolongation', 'restitution', 'mis_loyer_contentieux', 'prolongation_contentieux', 'restitution_contentieux')
-        or `affaire`.`nature` = 'vente')
+        or `affaire`.`nature` = 'vente');
 
 CREATE OR REPLACE
 ALGORITHM = UNDEFINED VIEW `factures_client` AS
@@ -78,7 +78,7 @@ where
     select
         `affaire_client`.`id_affaire`
     from
-        `affaire_client`)
+        `affaire_client`);
 
 CREATE OR REPLACE
 ALGORITHM = UNDEFINED VIEW `abonnement_client` AS
@@ -105,4 +105,24 @@ join `affaire` on
 where
     `affaire`.`etat` not in ('demande_refi', 'facture_refi')
     and (`commande`.`etat` in ('mis_loyer', 'prolongation', 'restitution', 'mis_loyer_contentieux', 'prolongation_contentieux', 'restitution_contentieux')
-        or `affaire`.`nature` = 'vente')
+        or `affaire`.`nature` = 'vente');
+
+
+ALTER TABLE `loyer_kilometrage` ADD `echeance` INT NOT NULL AFTER `id_affaire`, ADD `montant_ht` FLOAT(8,2) NOT NULL AFTER `echeance`;
+
+CREATE TABLE `restitution_anticipee` (
+  `id_loyer_kilometrage` mediumint(8) UNSIGNED NOT NULL,
+  `loyer` float(8,2) NOT NULL,
+  `kilometrage` mediumint(9) NOT NULL,
+  `id_affaire` mediumint(8) UNSIGNED NOT NULL,
+  `echeance` int(11) NOT NULL,
+  `montant_ht` float(8,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+ALTER TABLE `restitution_anticipee`
+  ADD PRIMARY KEY (`id_loyer_kilometrage`),
+  ADD KEY `id_affaire` (`id_affaire`);
+
+ALTER TABLE `restitution_anticipee` MODIFY `id_loyer_kilometrage` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `restitution_anticipee` ADD CONSTRAINT `restitution_anticipee_ibfk_1` FOREIGN KEY (`id_affaire`) REFERENCES `affaire` (`id_affaire`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
