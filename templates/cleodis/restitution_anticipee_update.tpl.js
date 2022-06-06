d10e83192964dbdce322952ef759548ced46e715
Morgan <mfleurquin@absystech.fr>
2022-06-06 10:33:40 +0200

    :bug: Fix probleme , en trop dans listing des champs

--- templates/cleodis/restitution_anticipee_update.tpl.js
+++ templates/cleodis/restitution_anticipee_update.tpl.js
@@ -3,7 +3,7 @@
 {$current_class=ATF::getClass("restitution_anticipee")}
 {$q=ATF::_s(pager)->getAndPrepare($pager)}
 {$fields=[
-	, "{$current_class->table}.echeance"
+	  "{$current_class->table}.echeance"
 	, "{$current_class->table}.montant_ht"
 ]}
 
