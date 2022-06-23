a479f5483ab29695d9295d22f7cb5aee31f4d48a
Morgan <mfleurquin@absystech.fr>
2022-06-23 12:00:42 +0200

    :hammer: Update SQL

--- sql/cleodis.sql
+++ sql/cleodis.sql
@@ -1,10 +1,12 @@
 ALTER TABLE `produit`
-ADD `assurance` FLOAT(6,3) UNSIGNED NOT NULL DEFAULT '0.00' AFTER `loyer`,
-ADD `frais_de_gestion` FLOAT(6,3) UNSIGNED NOT NULL DEFAULT '0.00' AFTER `assurance`,
-ADD `serenite` FLOAT(6,3) UNSIGNED NOT NULL DEFAULT '0.00' AFTER `frais_de_gestion`,
-ADD `maintenance` FLOAT(6,3) UNSIGNED NOT NULL DEFAULT '0.00' AFTER `serenite`,
-ADD `hotline` FLOAT(6,3) UNSIGNED NOT NULL DEFAULT '0.00' AFTER `maintenance`,
-ADD `supervision` FLOAT(6,3) UNSIGNED NOT NULL DEFAULT '0.00' AFTER `hotline`,
-ADD `support` FLOAT(6,3) UNSIGNED NOT NULL DEFAULT '0.00' AFTER `supervision`;
+ADD `assurance` FLOAT(6,3) NULL AFTER `loyer`,
+ADD `frais_de_gestion` FLOAT(6,3) NULL AFTER `assurance`,
+ADD `serenite` FLOAT(6,3) NULL AFTER `frais_de_gestion`,
+ADD `maintenance` FLOAT(6,3) NULL AFTER `serenite`,
+ADD `hotline` FLOAT(6,3) NULL AFTER `maintenance`,
+ADD `supervision` FLOAT(6,3) NULL AFTER `hotline`,
+ADD `support` FLOAT(6,3) NULL AFTER `supervision`;
+
+
 
 ALTER TABLE `produit` DROP `loyer_sans_tva`;
