0b00281d183223ca534d4666e90170757ba4d39a
Morgan <mfleurquin@absystech.fr>
2022-08-29 10:00:02 +0200

    :bug: Fix de l'affichage des champs du module agence

--- includes/agence.class.php
+++ includes/agence.class.php
@@ -3,25 +3,14 @@
 * @package Optima
 */
 class agence extends classes_optima {
-	public function __construct() { 
+	public function __construct() {
 		parent::__construct();
-		$this->table = __CLASS__;
-		$this->colonnes["fields_column"] = array(	
+		$this->table = "agence";
+		$this->colonnes["fields_column"] = array(
 			'agence.agence'
 			,'agence.ville'
 		);
-		$this->colonnes['panel']['coordonnees'] = array(
-			"adresse"
-			,"adresse_2"
-			,"adresse_3"
-			,"cp"
-			,"ville"
-			,"id_pays"
-			,"tel"
-			,"fax"
-		);
 		$this->fieldstructure();
-		$this->panels['coordonnees'] = array("visible"=>true);
 	}
 		/**
 	* Permet de récupérer la liste des agences pour telescope
