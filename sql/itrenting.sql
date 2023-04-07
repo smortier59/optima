5024780030db66315619a312cc7889a6f1fcb859
Morgan <mfleurquin@absystech.fr>
2023-04-07 10:33:44 +0200

    SQL

--- sql/itrenting.sql
+++ sql/itrenting.sql
@@ -1,3 +1,2 @@
 ALTER TABLE `societe` ADD `province` VARCHAR(200) NULL DEFAULT NULL AFTER `ville`;
 ALTER TABLE `societe` ADD `facturation_province` VARCHAR(200) NULL DEFAULT NULL AFTER `facturation_ville`;
-ALTER TABLE `societe` ADD `livraison_province` VARCHAR(200) NULL DEFAULT NULL AFTER `livraison_ville`;
