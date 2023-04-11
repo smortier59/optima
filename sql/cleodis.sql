5aa05878b666f1ed97f7ac879e8d2cbdfcb502ae
Morgan <mfleurquin@absystech.fr>
2023-04-11 09:52:10 +0200

    SQL

--- sql/cleodis.sql
+++ sql/cleodis.sql
@@ -1,4 +1,4 @@
 INSERT INTO `type_affaire` (`id_type_affaire`, `type_affaire`, `libelle_pdf`, `devis_template`, `contrat_template`, `assurance_sans_tva`)
 VALUES
-(NULL, 'SIMPLE FLUX', 'Cléodis', 'devis', 'contrat_simple_fux', 'non'),
-(NULL, 'SIMPLE START', 'Cléodis', 'devis', 'contrat_simple_start', 'non');
+(NULL, 'SIMPLE FLUX', 'Cléodis', 'devis', 'contrat_simpel_fux', 'non'),
+(NULL, 'SIMPLE START', 'Cléodis', 'devis', 'contrat_simpel_start', 'non');
