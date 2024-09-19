449acd32fc6cae13618da2feea27de72f2fe8b79
Morgan Fleurquin <contact@morgan-fleurquin.fr>
2024-09-19 09:44:50 +0200

    Génération facturation sur une période donnée

--- crontab/cleodis/cleodis_facturation.php
+++ crontab/cleodis/cleodis_facturation.php
@@ -4,10 +4,19 @@ include(dirname(__FILE__)."/../../global.inc.php");
 if(exec('whoami')!="apache"){
 	print_r("************************Penser à vérifier les droits de TEMP***********************************");
 }
+if ($_SERVER["argv"][2]) {
+	ATF::facturation()->facturationMensuelle(false, $_SERVER["argv"][2]);
+} else {
+	ATF::facturation()->facturationMensuelle();
+}
 
 
-ATF::facturation()->facturationMensuelle();
-
-if($_SERVER["argv"][1] !== "bdomplus" && $_SERVER["argv"][1] !== "go_abonnement") ATF::facturation()->facturationMensuelleRestitution();
+if($_SERVER["argv"][1] !== "bdomplus" && $_SERVER["argv"][1] !== "go_abonnement") {
+	if ($_SERVER["argv"][2]) {
+		ATF::facturation()->facturationMensuelleRestitution(false, $_SERVER["argv"][2]);
+	} else {
+		ATF::facturation()->facturationMensuelleRestitution();
+	}
+}
 
 ?>
